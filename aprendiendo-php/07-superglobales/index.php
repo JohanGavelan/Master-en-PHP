3443886f9463b0f67c65efdb979f5c1e5c58d4af
Johan Gavelan <johangavelan@gmail.com>
2020-05-14 07:48:41 -0500

    formularios get y post

---
+++ aprendiendo-php/07-superglobales/index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+// Variables superglobales
+
+// Variables de servidor
+
+echo '<h1>';
+
+echo $_SERVER['SERVER_ADDR'] . '<br>';
+echo $_SERVER['SERVER_NAME'] . '<br>';
+echo $_SERVER['SERVER_SOFTWARE'] . '<br>';
+echo $_SERVER['HTTP_USER_AGENT'] . '<br>';
+echo $_SERVER['REMOTE_ADDR'] . '<br>';
+
+echo '</h1>';   
