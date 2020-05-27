c79d54fa6ebb5b33606f388f649736939c6cc19b
Johan Gavelan <johangavelan@gmail.com>
2020-05-27 18:30:28 -0500

    Clases de Diseño de base de datos terminada

--- aprendiendo-sql/03-diseño/blog.sql
+++ aprendiendo-sql/03-diseño/blog.sql
@@ -5,14 +5,15 @@ apellidos VARCHAR(100) NOT NULL,
 email VARCHAR(255) NOT NULL,
 password VARCHAR(255) NOT NULL,
 fecha DATE NOT NULL,
-CONSTRAINT pk_usuarios PRIMARY KEY(ID);
-);
+CONSTRAINT pk_usuarios PRIMARY KEY(ID),
+CONSTRAINT uq_email UNIQUE(email)
+)ENGINE = InnoDb;
 
 CREATE TABLE categorias (
 ID int(255) AUTO_INCREMENT NOT NULL,
 nombre VARCHAR(100) NOT NULL,
 CONSTRAINT pk_categorias PRIMARY KEY(ID)  
-);
+)ENGINE = InnoDb;
 
 CREATE TABLE entradas (
 ID              int(255) AUTO_INCREMENT NOT NULL,
@@ -24,4 +25,4 @@ fecha           date NOT NULL,
 CONSTRAINT pk_entradas PRIMARY KEY(ID),
 CONSTRAINT fk_entrada_usuario FOREIGN KEY(usuario_id) REFERENCES usuarios(ID),
 CONSTRAINT fk_entrada_categoria FOREIGN KEY (categoria_id) REFERENCES categorias(ID)
-);
+)ENGINE = InnoDb;  
