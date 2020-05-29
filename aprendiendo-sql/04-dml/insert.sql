6c18b881e37c43f476cd42a34b4366e13645ec86
Johan Gavelan <johangavelan@gmail.com>
2020-05-28 19:59:16 -0500

    Sección DML terminada

---
+++ aprendiendo-sql/04-dml/insert.sql
@@ -0,0 +1,10 @@
+#INSERTAR NUEVOS REGISTROS 
+
+INSERT INTO usuarios VALUES (null, 'Johan', 'Gavelan', 'johangavelan@gmail.com', '23232', '2020-05-28');
+INSERT INTO usuarios VALUES (null, 'Agustin', 'Lopez', 'example@gmail.com', '4334sds', '2020-05-29');
+INSERT INTO usuarios VALUES (null, 'Paco', 'Rodriguez', 'example2@gmail.com', '23452', '2020-05-30');
+
+
+#INSERTAR FILAS SOLO CON CIERTAS COLUMNAS
+
+INSERT INTO usuarios (email, password) VALUES ('example3@gmail.com', 'dreqr234');
