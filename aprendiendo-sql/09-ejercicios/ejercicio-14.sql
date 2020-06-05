0b91853a6ae9fd79fd12fcc5fb2f2c36015d91ae
Johan Gavelan <johangavelan@gmail.com>
2020-06-04 19:39:41 -0500

    Ejercicio 14 resuelto por el profesor

---
+++ aprendiendo-sql/09-ejercicios/ejercicio-14.sql
@@ -0,0 +1,6 @@
+#VISUALIZAR LAS UNIDADES TOTALES VENDIDAS DE CADA COCHE A CADA CLIENTE, MOSTRANDO EL NOMBRE DEL PRODUCTO, EL NÚMERO DEL CLIENTE Y LA SUMA DE UNIDADES
+
+SELECT co.modelo AS 'COCHE', cl.nombre 'CLIENTE', SUM(e.cantidad) AS 'UNIDADES' FROM encargos e 
+INNER JOIN coches co ON co.id = e.coche_id
+INNER JOIN clientes cl ON cl.id = e.cliente_id
+GROUP BY e.coche_id, e.cliente_id;
