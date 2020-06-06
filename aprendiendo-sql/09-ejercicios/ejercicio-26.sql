ad20d8f42d2f6c3e11728cf99bb330ee64157ea4
Johan Gavelan <johangavelan@gmail.com>
2020-06-05 21:56:13 -0500

    Ejercicio 26 resuelto por el instructor

---
+++ aprendiendo-sql/09-ejercicios/ejercicio-26.sql
@@ -0,0 +1,4 @@
+#SACAR LOS VENDEDORES QUE TIENEN JEFE Y SACAR EL NOMBRE DEL JEFE
+
+SELECT CONCAT(v1.nombre, ' ', v1.apellidos) AS 'Vendedor', CONCAT(v2.nombre, ' ', v2.apellidos) AS 'Jefe' FROM vendedores v1
+INNER JOIN vendedores v2 ON v1.jefe = v2.id; 
