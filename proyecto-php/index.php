7ab8d380baf0b5d23274227c712c2b001f8ca865
Johan Gavelan <johangavelan@gmail.com>
2020-06-12 09:52:54 -0500

    Login hecho

--- proyecto-php/index.php
+++ proyecto-php/index.php
@@ -1,4 +1,4 @@
-<?php require_once 'includes/header.php' ?>
+<?php require_once 'includes/cabecera.php' ?>
 
 <?php require_once 'includes/lateral.php' ?>
 
---
+++ proyecto-php/login.php
@@ -0,0 +1,48 @@
+<?php
+
+//CONECTARSE A LA BASE DE DATOS E INICIAR SESIÓN
+
+require_once 'includes/conexion.php';
+
+//RECOGER DATOS DEL FORMULARIO
+
+if (isset($_POST)) {
+    $email = trim($_POST['email']);
+    $password = $_POST['password'];
+
+    //Consulta para comprobar credenciales del usuario
+    $sql = "SELECT * FROM usuarios WHERE email = '$email' LIMIT 1;";
+    $login = mysqli_query($db, $sql);
+
+    if ($login && mysqli_num_rows($login) == 1) {
+        $usuario = mysqli_fetch_assoc($login);
+
+        //Comprobar la contraseña
+        $verify = password_verify($password, $usuario['password']);
+
+        if ($verify) {
+            //Utilizar una sesión para guardar los datos del usuario logueado
+            $_SESSION['usuario'] = $usuario;
+
+            if (isset($_SESSION['error_login'])) {
+                session_unset();
+            }
+        }
+
+        else {
+            $_SESSION['error_login'] = "Login incorrecto";
+        }
+    }
+
+    else {
+        $_SESSION['error_login'] = "Login incorrecto";
+    }
+    
+}
+
+header('Location: index.php');
+
+
+
+
+
