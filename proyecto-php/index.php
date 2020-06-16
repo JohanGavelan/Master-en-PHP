a941b454147bff4db847dff6e2d4ca4746365b78
Johan Gavelan <johangavelan@gmail.com>
2020-06-15 21:19:10 -0500

    Configurando entradas

--- proyecto-php/index.php
+++ proyecto-php/index.php
@@ -1,33 +1,27 @@
 <?php require_once 'includes/cabecera.php' ?>
-
 <?php require_once 'includes/lateral.php' ?>
 
 <div id="principal">
     <h1>Últimas entradas</h1>
-    <article class="entrada">
-        <a href="">
-            <h2>Título de mi entrada</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita excepturi quae neque quas eaque maiores perferendis aut quam omnis, doloremque porro odit assumenda dicta, consequuntur provident repellat, non earum deserunt.</p>
+    <?php
+    $entradas = conseguirUltimasEntradas($db);
+    if (!empty($entradas)) :
+        while ($entrada = mysqli_fetch_assoc($entradas)) :
+    ?>
+        <article class="entrada">
+        <a href="#">
+            <h2><?= $entrada['titulo']?></h2>
+            <p>
+                <?= $entrada['descripcion']?>
+            </p>
         </a>
+        
     </article>
-    <article class="entrada">
-        <a href="">
-            <h2>Título de mi entrada</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita excepturi quae neque quas eaque maiores perferendis aut quam omnis, doloremque porro odit assumenda dicta, consequuntur provident repellat, non earum deserunt.</p>
-        </a>
-    </article>
-    <article class="entrada">
-        <a href="">
-            <h2>Título de mi entrada</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita excepturi quae neque quas eaque maiores perferendis aut quam omnis, doloremque porro odit assumenda dicta, consequuntur provident repellat, non earum deserunt.</p>
-        </a>
-    </article>
-    <article class="entrada">
-        <a href="">
-            <h2>Título de mi entrada</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita excepturi quae neque quas eaque maiores perferendis aut quam omnis, doloremque porro odit assumenda dicta, consequuntur provident repellat, non earum deserunt.</p>
-        </a>
-    </article>
+    <?php
+        endwhile;
+    endif;
+    ?>
+    
     <div id="btn">
         <a href="">Ver todas las entradas</a>
     </div>
